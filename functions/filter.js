99281ab1a6d01c3f6ecf912bcfbf99f2c4f09bbf
Ameer Badri <ameerbadri@gmail.com>
2020-04-09 18:26:36 +0100

    added filter function

--- functions/filter.js
+++ functions/filter.js
@@ -1,7 +1,13 @@
 exports.handler = function(event, context, callback) {
     console.log(`{\ncontext: ${JSON.stringify(context,null,2)},\nevent: ${JSON.stringify(event,null,2)}\n}`)
+    if (event.headers['client-ip'] == '86.0.19.200') {
+        callback(null, {
+            statusCode: 200,
+            body: `Your IP address ${event.headers['client-ip']}`
+            });   
+    }
     callback(null, {
-    statusCode: 200,
-    body: `Your IP address ${event.headers['client-ip']}`
+    statusCode: 403,
+    body: `Your IP address ${event.headers['client-ip']} is forbidden`
     });
   }
