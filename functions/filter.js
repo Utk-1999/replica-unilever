e5ff3291d2f861821106574c4bfce2728fdde72d
Ameer Badri <ameerbadri@gmail.com>
2020-04-10 12:05:39 +0100

    added filter function

--- functions/filter.js
+++ functions/filter.js
@@ -1,13 +1,14 @@
 exports.handler = function(event, context, callback) {
     console.log(`{\ncontext: ${JSON.stringify(context,null,2)},\nevent: ${JSON.stringify(event,null,2)}\n}`)
+    const response = ""
     if (event.headers['client-ip'] == '86.0.19.200') {
-        callback(null, {
+        response =  {
             statusCode: 200,
             body: `Your IP address ${event.headers['client-ip']} is forbidden` 
-            });   
+            };   
     }
     const redirectURI = 'https://www.unilever.com/:splat'
-    const response = {
+    response = {
         statusCode: 200,
         headers: {
             Location: redirectURI
