a595049d057f88af079afb1d8927a6663f36bdae
Ameer Badri <ameerbadri@gmail.com>
2020-04-10 19:10:02 +0100

    modified filter function

--- functions/filter.js
+++ functions/filter.js
@@ -5,23 +5,21 @@ exports.handler = function(event, context, callback) {
     if (event.headers['client-ip'] == '86.0.19.200') {
         redirectURI = '/forbidden.html'
         response =  {
-            statusCode: 200,
+            statusCode: 302,
             headers: {
                 Location: redirectURI,
                 'Cache-Control': 'no-cache'
             },
-            body: ''
             };   
     }
     else {
         redirectURI = '/allowed.html'
         response = {
-            statusCode: 200,
+            statusCode: 302,
             headers: {
                 Location: redirectURI,
                 'Cache-Control': 'no-cache'
             },
-            body: ''
     }
 }
     console.log(response);
