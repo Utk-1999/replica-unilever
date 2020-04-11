21424f2c521d9a281c9af2c231c8bef6eb8e4f0e
Ameer Badri <ameerbadri@gmail.com>
2020-04-11 01:05:16 +0100

    modified filter function

--- functions/filter.js
+++ functions/filter.js
@@ -1,7 +1,7 @@
 const cookie = require("cookie");
 const axios = require("axios");
-const jwt = require("jwt")
-const uuid = require("uuid/v4")
+const jwt = require("jwt");
+const uuid = require("uuid/v4");
 
 exports.handler = function(event, context, callback) {
     console.log(`{\ncontext: ${JSON.stringify(context,null,2)},\nevent: ${JSON.stringify(event,null,2)}\n}`)
