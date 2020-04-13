2d8699617077cf23f5e32d4caac57fc6c5178f28
Ameer Badri <ameerbadri@gmail.com>
2020-04-13 13:43:16 +0100

    changes to function

--- functions/filter.js
+++ functions/filter.js
@@ -1,12 +1,16 @@
 const fetch = require("node-fetch");
 const API_ENDPOINT = "https://icanhazdadjoke.com/";
 
-exports.handler = async (event, context) => {
-  return fetch(API_ENDPOINT, { headers: { "Accept": "application/json" } })
+exports.handler = function(event, context, callback) {
+  var response = {}
+  console.log(`{\ncontext: ${JSON.stringify(context,null,2)},\nevent: ${JSON.stringify(event,null,2)}\n}`)
+ 
+  response = fetch(API_ENDPOINT, { headers: { "Accept": "application/json" } })
     .then(response => response.json())
     .then(data => ({
       statusCode: 200,
       body: data.joke
     }))
     .catch(error => ({ statusCode: 422, body: String(error) }));
+  callback (null, response);  
 };
