ed52e70a51701b89a4879cb126accdfb8225fc6f
Ameer Badri <ameerbadri@gmail.com>
2020-04-14 00:04:48 +0100

    added new functions using netlify dev

--- functions/filter-ip/filter-ip.js
+++ functions/filter-ip/filter-ip.js
@@ -1,4 +1,8 @@
 const fetch = require('node-fetch')
+const jwt = require("jsonwebtoken");
+const uuidv4 = require("uuid/v4");
+const cookie = require("cookie");
+
 exports.handler = async (event, context) => {
     console.log(`{\ncontext: ${JSON.stringify(context,null,2)},\nevent: ${JSON.stringify(event,null,2)}\n}`)
     var response = ""
